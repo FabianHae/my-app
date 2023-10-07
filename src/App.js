6a4e9a6a1e362510cbbfd9b7a8f678b4cfb4b99f
Fabian <f.haeckermann@oldtimerparts.de>
2023-10-08 00:04:03 +0200

    Hola Fabi este es mi primer commit en dev

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Ciao lu Edit <code>src/App.js</code> and save to reload.
+          Ciao este es un cambio en dev lu Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"
