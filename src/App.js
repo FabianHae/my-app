73d5de956da4129d97fa47bf760dfebbd3d44b58
Luisa-Rodriguez <lrodriguezm1991@gmail.com>
2023-10-08 12:11:03 -0500

    cambio en dev by luisa

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Korije ezto !!!! cambio desde compu luisa <code>src/App.js</code> and save to reload.
+          Korije ezto !!!! que lo cambie su abuela luisa <code>src/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"
