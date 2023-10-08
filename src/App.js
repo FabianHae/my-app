a0222382f7fdca8b3c93d6ceca416257b231e3ee
Luisa-Rodriguez <lrodriguezm1991@gmail.com>
2023-10-08 12:19:57 -0500

    cambio en header by luisa

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Korije ezto !!!! que lo cambie su abuela luisa <code>src/App.js</code> and save to reload.
+          primer cambio en header!! <code>src/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"
